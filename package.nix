ffd4d386f38c35d09fd278fc5040161e3c3ca5f6
Fernando Ayats <ayatsfer@gmail.com>
2024-08-30 13:35:17 +0200

    Remove lib.fix
    Co-authored-by: =?UTF-8?q?Nikodem=20Rabuli=C5=84ski?= <nikodem@rabulinski.com>

--- package.nix
+++ package.nix
@@ -3,29 +3,28 @@
   cmake,
   stdenv,
 }:
-lib.fix (self:
-    stdenv.mkDerivation {
-      name = "in-nix";
+stdenv.mkDerivation (final: {
+  name = "in-nix";
 
-      src = lib.fileset.toSource {
-        root = ./.;
-        fileset = lib.fileset.unions [
-          ./in-nix.c
-          ./CMakeLists.txt
-        ];
-      };
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset = lib.fileset.unions [
+      ./in-nix.c
+      ./CMakeLists.txt
+    ];
+  };
 
-      nativeBuildInputs = [
-        cmake
-      ];
+  nativeBuildInputs = [
+    cmake
+  ];
 
-      passthru.patchNix = nix:
-        nix.overrideAttrs (old: {
-          postInstallCheck =
-            (old.postInstallCheck or "")
-            + ''
-              patchelf --add-needed libin-nix.so $out/bin/nix
-              patchelf --set-rpath "$(patchelf --print-rpath $out/bin/nix):${self}/lib" $out/bin/nix
-            '';
-        });
-    })
+  passthru.patchNix = nix:
+    nix.overrideAttrs (old: {
+      postInstallCheck =
+        (old.postInstallCheck or "")
+        + ''
+          patchelf --add-needed libin-nix.so $out/bin/nix
+          patchelf --set-rpath "$(patchelf --print-rpath $out/bin/nix):${final.finalPackage}/lib" $out/bin/nix
+        '';
+    });
+})
