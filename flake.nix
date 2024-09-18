66982d6a83ab1a7f0b674cc5d335b2141a43f811
Fernando Ayats <ayatsfer@gmail.com>
2024-09-18 14:29:46 +0200

    add aarch64-linux output
    Fixes #2

--- flake.nix
+++ flake.nix
@@ -12,6 +12,11 @@
       nix = self.packages.x86_64-linux.default.patchNix nixpkgs.legacyPackages.x86_64-linux.nix;
     };
 
+    packages.aarch64-linux = {
+      default = nixpkgs.legacyPackages.aarch64-linux.callPackage ./package.nix {};
+      nix = self.packages.aarch64-linux.default.patchNix nixpkgs.legacyPackages.aarch64-linux.nix;
+    };
+
     devShells.x86_64-linux.default = with nixpkgs.legacyPackages.x86_64-linux;
       mkShell {
         packages = [
